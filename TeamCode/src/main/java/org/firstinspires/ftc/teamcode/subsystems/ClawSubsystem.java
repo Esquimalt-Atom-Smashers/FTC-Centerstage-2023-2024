bbc08c7a69535e43628f3ee92c21ba932a029d5e
Elliot Fox <efoxyyj@gmail.com>
2023-09-14 22:59:12 -0700

    Fixed ClawSubsystem (To Lottie and Moss: Will explain why, Elliot)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ClawSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ClawSubsystem.java
@@ -1,41 +1,40 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
-import com.arcrobotics.ftclib.hardware.ServoEx;
-import com.arcrobotics.ftclib.hardware.SimpleServo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import static org.firstinspires.ftc.teamcode.Constants.ArmConstants.*;
 
 public class ClawSubsystem {
-    
+
     private final Servo claw1;
     private final Servo claw2;
 
     public ClawSubsystem(HardwareMap hardwareMap, Telemetry telemetry)
     {
-        claw1 = hardwareMap.servo.get(Constants.ArmConstants.CLAW1_SERVO_MOTOR_NAME);
-        claw2 = hardwareMap.servo.get(Constants.ArmConstants.CLAW2_SERVO_MOTOR_NAME);
+        claw1 = hardwareMap.servo.get(CLAW1_SERVO_MOTOR_NAME);
+        claw2 = hardwareMap.servo.get(CLAW2_SERVO_MOTOR_NAME);
     }
 
     public void openClaw1()
     {
-        claw1.setPosition(/*Hard code values*/);
+        claw1.setPosition(OPEN_POSITION);
     }
 
     public void openClaw2()
     {
-        claw2.setPosition(/*Hard code values*/);
+        claw2.setPosition(OPEN_POSITION);
     }
 
     public void closeClaw1()
     {
-        claw1.setPosition(/*Hard code values*/);
+        claw1.setPosition(CLOSE_POSITION);
     }  
 
     public void closeClaw2()
     {
-        claw2.setPosition(/*Hard code values*/);
+        claw2.setPosition(CLOSE_POSITION);
     }  
 
     public Servo getClaw1()
