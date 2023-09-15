e6f3103716784c1bbd9996ad2cd19f7b09f58a83
Elliot Fox <efoxyyj@gmail.com>
2023-09-14 23:00:39 -0700

    Added Robot centric drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
 //This class holds all constants, the reason we do this is so we can import all the constants 'statically.'
 public class Constants {
     public static class DriveConstants {
@@ -9,10 +11,20 @@ public class Constants {
             - booleans(USE_FIELD_CENTRIC)
             - limits(MAX_SPEED_M_S)
         */
+
+        public static float INPUT_MULTIPLIER = 0.8f;
+
         public static String FRONT_LEFT_MOTOR_NAME = " ";
-        public static String REAR_LEFT_MOTOR_NAME = " ";
         public static String FRONT_RIGHT_MOTOR_NAME = " ";
+        public static String REAR_LEFT_MOTOR_NAME = " ";
         public static String REAR_RIGHT_MOTOR_NAME = " ";
+
+        public static DcMotorSimple.Direction FRONT_LEFT_MOTOR_DIRECTION = DcMotorSimple.Direction.FORWARD;
+        public static DcMotorSimple.Direction FRONT_RIGHT_MOTOR_DIRECTION = DcMotorSimple.Direction.FORWARD;
+        public static DcMotorSimple.Direction REAR_LEFT_MOTOR_DIRECTION = DcMotorSimple.Direction.FORWARD;
+        public static DcMotorSimple.Direction REAR_RIGHT_MOTOR_DIRECTION = DcMotorSimple.Direction.FORWARD;
+
+        public static String IMU_NAME = "imu";
 
         public static boolean FIELD_CENTRIC = false;
         public static boolean SCALED = false;
@@ -26,6 +38,9 @@ public class Constants {
     {
         public static String CLAW1_SERVO_MOTOR_NAME = " ";
         public static String CLAW2_SERVO_MOTOR_NAME = " ";
+
+        public static double OPEN_POSITION = -1;
+        public static double CLOSE_POSITION = -1;
     }
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveSubsystem.java
@@ -1,65 +1,112 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
-
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.Range;
+
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
-// import android.os.Build;
+import static org.firstinspires.ftc.teamcode.Constants.DriveConstants.*;
 
-// import com.qualcomm.hardware.bosch.BNO055IMU;
-// import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
-// import com.qualcomm.robotcore.hardware.DcMotorSimple;
-// import com.qualcomm.robotcore.util.Range;
-
-// import org.firstinspires.ftc.robotcore.external.Predicate;
-// import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-// import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
-// import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
-// import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
-
-// import java.nio.channels.ScatteringByteChannel;
-// import java.util.Arrays;
-
-// import teamcode.internal.util.EncoderConstants;
-
+import java.util.Arrays;
 
 public class DriveSubsystem {
     //Declare hardware here.
-    private final DcMotor frontRightMotor, frontLeftMotor, rearRightMotor, rearLeftMotor;
+    private final DcMotor frontLeftMotor, frontRightMotor, rearLeftMotor, rearRightMotor;
+    private final DcMotor[] motors;
+
+    // TODO: Check what kind of imu we have (think it's the BNO055IMU)
+    private final BNO055IMU imu;
 
     //Initialize hardware here
-    public DriveSubsystem(HardwareMap hardwareMap, Telemetry, telemetry) {
+    public DriveSubsystem(HardwareMap hardwareMap, Telemetry telemetry) {
         // Initialize the motors
-        frontRightMotor = harwareMap.dcMotor.get(Constants.DriveConstants.FRONT_RIGHT_MOTOR);
-        frontLeftMotor = harwareMap.dcMotor.get(Constants.DriveConstants.FRONT_LEFT_MOTOR);
-        rearRightMotor = harwareMap.dcMotor.get(Constants.DriveConstants.REAR_RIGHT_MOTOR);
-        rearLeftMotor = harwareMap.dcMotor.get(Constants.DriveConstants.REAR_LEFT_MOTOR);
+        frontLeftMotor = hardwareMap.dcMotor.get(FRONT_LEFT_MOTOR_NAME);
+        frontRightMotor = hardwareMap.dcMotor.get(FRONT_RIGHT_MOTOR_NAME);
+        rearLeftMotor = hardwareMap.dcMotor.get(REAR_LEFT_MOTOR_NAME);
+        rearRightMotor = hardwareMap.dcMotor.get(REAR_RIGHT_MOTOR_NAME);
+
+        motors = new DcMotor[]{frontRightMotor, frontLeftMotor, rearLeftMotor, rearRightMotor};
+
+        frontLeftMotor.setDirection(FRONT_LEFT_MOTOR_DIRECTION);
+        frontRightMotor.setDirection(FRONT_RIGHT_MOTOR_DIRECTION);
+        rearLeftMotor.setDirection(REAR_LEFT_MOTOR_DIRECTION);
+        rearRightMotor.setDirection(REAR_RIGHT_MOTOR_DIRECTION);
+
+        Arrays.stream(motors).forEach(motor -> motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE));
+        setMotorMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        setMotorMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        // Initialize the imu
+        imu = hardwareMap.get(BNO055IMU.class, IMU_NAME);
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.mode = BNO055IMU.SensorMode.IMU;
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        imu.initialize(parameters);
+
+
     }
 
     //Define methods that control the bot down here.
 
-    public void drive(float forward, float strafe, float turn)
-    {
-        if (Constants.DriveConstants.FIELD_CENTRIC) {
+    public void drive(float forward, float strafe, float turn) {
+        if (FIELD_CENTRIC) {
             // Field centric drive
             // TODO: Field centric drive
         }
         else {
-            // Normal drive (probably not going to be used)
-            // TODO: (low) Robot centric drive
+            // Robot centric drive
+            frontLeftMotor.setPower(scaleInput(forward + strafe + turn));
+            frontRightMotor.setPower(scaleInput(forward - strafe - turn));
+            rearLeftMotor.setPower(scaleInput(forward - strafe + turn));
+            rearRightMotor.setPower(scaleInput(forward + strafe - turn));
         }
+
+    }
+
+    // Auto drive
+    public void drive() {
+
+    }
+
+    // Auto strafe
+    public void strafe() {
+
+    }
+
+    // Used for autonomous driving
+    public void driveToPosition() {
+
+    }
+
+    public void stop() {
+        Arrays.stream(motors).forEach(motor -> motor.setPower(0));
     }
 
     //Define lower-level methods here. (Methods that are private or work behind the scenes)
 
+    private void setMotorMode(DcMotor.RunMode runMode) {
+        Arrays.stream(motors).forEach(motor -> motor.setMode(runMode));
+    }
+
     // Take the input (forward, strafe, turn) and scale it so that moving the joystick halfway doesn't use half power
-    private float scaleInput(float input)
-    {
-        // TODO: Create a scaling formula
-        return input
+    private float scaleInput(float input) {
+        if (SCALED) {
+            // TODO: Create a scaling formula
+            return  Range.clip(input * INPUT_MULTIPLIER, -1, 1);
+        }
+        else {
+            return Range.clip(input * INPUT_MULTIPLIER, -1, 1);
+        }
     }
 
     //Define getter/setter's here.
-    
+
+    public BNO055IMU getGyro() {
+        return imu;
+    }
 }
