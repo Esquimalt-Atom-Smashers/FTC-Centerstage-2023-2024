e53b4562022a4cb26a890e8f29ce23ec60e35bd3
Elliot Fox <1209756@sd61learn.ca>
2023-09-18 17:03:46 -0700

    Elbow system made a bit of progress

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ElbowSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ElbowSubsystem.java
@@ -1,9 +1,10 @@
 package org.firstinspires.ftc.teamcode.subsystems;
-
+import static org.firstinspires.ftc.teamcode.Constants.LinearSlideConstants.PULSES_PER_INCH;
 import static org.firstinspires.ftc.teamcode.Constants.WristConstants.WRIST_SERVO_MOTOR_NAME;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.MotorControlAlgorithm;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -11,7 +12,10 @@ import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import static org.firstinspires.ftc.teamcode.Constants.ElbowConstants.*;
 
+import android.os.Debug;
+
 public class ElbowSubsystem {
+
     // One DcMotorEx
     // Method to spin the motor clockwise
     // Method to spin the motor counter clockwise
@@ -19,11 +23,11 @@ public class ElbowSubsystem {
     // Logic to limit the motor from going too high
     // Lower level method using PIDF to accurately set the position of the motor
 
-    public DcMotor elbowMotor;
+    public DcMotorEx elbowMotor;
 
     public ElbowSubsystem(HardwareMap hardwareMap, Telemetry telemetry)
     {
-        elbowMotor = hardwareMap.dcMotor.get(ELBOW_DC_MOTOR_NAME);
+        elbowMotor = hardwareMap.get(DcMotorEx.class, ELBOW_DC_MOTOR_NAME);
     }
     public void spinMotorClockwise(){
 
@@ -38,6 +42,40 @@ public class ElbowSubsystem {
     }
 
     public void setMotorPosition(){
+        elbowMotor.setTargetPosition((int)MOTOR_POSITION);
+    }
 
+    public enum TargetMotorPosition {
+        // The number of inches
+        DEFAULT(0),
+        LOW(-1),
+        MEDIUM(-1),
+        HIGH(-1);
+
+        private int inches;
+
+        TargetMotorPosition(int inches) {
+            this.inches = inches;
+        }
+        int getTarget() {
+            return (int) (inches * PULSES_PER_INCH);
+        }
+    }
+
+    public enum TargetArmPosition {
+        // The number of inches
+        DEFAULT(0),
+        LOW(-1),
+        MEDIUM(-1),
+        HIGH(-1);
+
+        private int inches;
+
+        TargetArmPosition(int inches) {
+            this.inches = inches;
+        }
+        int getTarget() {
+            return (int) (inches * PULSES_PER_INCH);
+        }
     }
 }
