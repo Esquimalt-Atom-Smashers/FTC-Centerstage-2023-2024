397bbbf7615b9deda489f3f5200acff16e1be0a7
Elliot Fox <1209756@sd61learn.ca>
2023-09-27 16:35:29 -0700

    We created the drone subsystem and made drone subsystem related constants. The drone subsystem consists of three methods: to stop, rotate forward and rotate backward the drone servo.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DroneSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DroneSubsystem.java
@@ -1,9 +1,17 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.arcrobotics.ftclib.hardware.ServoEx;
+import com.arcrobotics.ftclib.hardware.SimpleServo;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+import static org.firstinspires.ftc.teamcode.Constants.DroneConstants.*;
+
 public class DroneSubsystem {
     // One ServoEx
-    // Program the servo to continuous mode (use the servo programmer)
-
     // Method to rotate the servo
     // Method to stop the servo
     // 0 -> 100% back
@@ -11,4 +19,26 @@ public class DroneSubsystem {
     // .5 -> 0% power
     // 0.75 -> 50% forward
     // 1 -> 100% forward
+    // Program the servo to continuous mode (use the servo programmer)
+
+    private final ServoEx droneServo;
+
+    public DroneSubsystem(HardwareMap hardwareMap, Telemetry telemetry) {
+        // Initialize the servo
+        droneServo = new SimpleServo(hardwareMap, DRONE_SERVO_NAME, MIN_POSITION, MAX_POSITION);
+    }
+
+    public void rotateServoForward()
+    {
+        droneServo.turnToAngle(FORWARD_DEGREE);
+    }
+
+    public void stopServo()
+    {
+        droneServo.turnToAngle(STOP_DEGREE);
+    }
+    public void rotateServoBackward()
+    {
+        droneServo.turnToAngle(BACKWARD_DEGREE);
+    }
 }
